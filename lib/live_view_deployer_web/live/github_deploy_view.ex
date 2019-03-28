b385ed096bf7e02362f387d6c66eaacac8cfad57
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-03-27 20:15:42 -0700

    Add button and event handler

--- lib/live_view_deployer_web/live/github_deploy_view.ex
+++ lib/live_view_deployer_web/live/github_deploy_view.ex
@@ -9,4 +9,9 @@ defmodule LiveViewDeployerWeb.GithubDeployView do
   def mount(_session, socket) do
     {:ok, assign(socket, deploy_step: "Ready!")}
   end
+
+  def handle_event("github_deploy", _value, socket) do
+    # do the deploy process
+    {:noreply, assign(socket, deploy_step: "Starting deploy...")}
+  end
 end
