3e2132cd404dc591f8efd502731a88b3d6eb1b03
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-03-27 20:26:40 -0700

    Add dummy events to push new messages to the UI

--- lib/live_view_deployer.ex
+++ lib/live_view_deployer.ex
@@ -6,4 +6,22 @@ defmodule LiveViewDeployer do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  def start_deploy do
+    :ok
+  end
+
+  def create_org do
+    :timer.sleep(1000)
+    {:ok, "Test Org"}
+  end
+
+  def create_repo(org) do
+    :timer.sleep(1000)
+    {:ok, "Test Repo : #{org}"}
+  end
+
+  def push_contents(repo) do
+    :timer.sleep(1000)
+    :ok
+  end
 end
--- lib/live_view_deployer_web/live/github_deploy_view.ex
+++ lib/live_view_deployer_web/live/github_deploy_view.ex
@@ -11,7 +11,30 @@ defmodule LiveViewDeployerWeb.GithubDeployView do
   end
 
   def handle_event("github_deploy", _value, socket) do
-    # do the deploy process
+    :ok = LiveViewDeployer.start_deploy()
+    send(self(), :create_org)
     {:noreply, assign(socket, deploy_step: "Starting deploy...")}
+  end
+
+  def handle_info(:create_org, socket) do
+    {:ok, org} = LiveViewDeployer.create_org()
+    send(self(), {:create_repo, org})
+    {:noreply, assign(socket, deploy_step: "Creating GitHub org...")}
+  end
+
+  def handle_info({:create_repo, org}, socket) do
+    {:ok, repo} = LiveViewDeployer.create_repo(org)
+    send(self(), {:push_contents, repo})
+    {:noreply, assign(socket, deploy_step: "Creating GitHub repo...")}
+  end
+
+  def handle_info({:push_contents, repo}, socket) do
+    :ok = LiveViewDeployer.push_contents(repo)
+    send(self(), :done)
+    {:noreply, assign(socket, deploy_step: "Pushing to repo...")}
+  end
+
+  def handle_info(:done, socket) do
+    {:noreply, assign(socket, deploy_step: "Done!")}
   end
 end
